6fabd79f8893cc76caa998ed21363985720c14db
venkatsr16 <venkat5494@gmail.com>
2017-05-31 21:00:08 +0530

    Diamond Pattern

---
+++ DiamondPattern.java
@@ -0,0 +1,9 @@
+public class DiamondPattern {
+	public static void main(String args[]) {
+		System.out.println("  a  ");
+		System.out.println(" aba ");
+		System.out.println("abcba");
+		System.out.println(" aba ");
+		System.out.println("  a  ");
+	}
+}
